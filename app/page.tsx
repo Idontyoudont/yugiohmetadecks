e575ce67eba3648e847230e8d0c5cd23b5c17e24
Marc de Jong <marctradetracker@Marcs-MacBook-Pro.local>
2026-05-05 10:00:22 +0200

    Show curated imported decks in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import { DeckViewer } from "../components/DeckViewer";
 import { decks } from "../data/decks";
+import { curatedDeckIdSet } from "../data/curatedDeckIds.generated";
 import { importedDecks } from "../data/importedDecks.generated";
 
 export default function Home() {
-  return <DeckViewer decks={[...decks, ...importedDecks]} />;
+  const curatedImportedDecks = importedDecks.filter((deck) =>
+    curatedDeckIdSet.has(deck.id)
+  );
+
+  return <DeckViewer decks={[...decks, ...curatedImportedDecks]} />;
 }
